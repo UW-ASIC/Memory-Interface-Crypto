0cacc1ebc001c634c0cacaeab07ba06d4b26ea20
HarryWang <zuhanwang@outlook.com>
2025-10-30 19:57:32 -0400

    Added more detail to spi_controller comment

--- src/spi_controller.v
+++ src/spi_controller.v
@@ -4,7 +4,8 @@ Dual SPI/QSPI connection to the flash.
 
 The transaction FSM will tell us what to send to the SPI flash, we just facilitate the transfer of that command/address/data through the SPI port
 
-We’re responsible for things like asserting CS low, start driving SLCK, shift data out from MISO, read in data from MOSI, sets bit flag to “done” once transaction is complete
+We’re responsible for SPI things like asserting CS low, start driving SLCK, shift data out from MISO, read in data from MOSI.
+We also responsible setting our internal bit flag to “done” once transaction is complete to tell the status poller we can keep going
 
 Clock Divider / Shifting FSM is already partially implemented by Ryan
 
