d8215ebff16d0da70112671a2c87d05230bc7231
Joshua Ma <joshuama48@gmail.com>
2025-11-22 19:13:20 -0500

    uhhh maybe done uhhh

--- src/mem_spi_controller.v
+++ src/mem_spi_controller.v
@@ -47,13 +47,8 @@ end
 
 localparam DIVIDER = 1; //divider == 1 means half frequency, divider == 2 means quarter frequency 
 
-reg out_sclk_prev = 0;
-reg out_sclk_counter;
-wire out_sclk_rising;
-wire out_sclk_falling;
+reg [$clog2(DIVIDER)-1 : 0]out_sclk_counter;
 
-assign out_sclk_rising = (out_sclk == 1 && out_sclk_prev == 0);
-assign out_sclk_falling = (out_sclk == 0 && out_sclk_prev == 1);
 wire [4:0] num_cycles;
 reg [4:0] cycle_count;
 assign num_cycles = (quad_enable) ? 4'b0010 : 4'b1000; //2 cycles if quad enable, 8 cycles if quad enable
@@ -64,7 +59,6 @@ localparam FINISHED = 2'b10;
 
 reg internal_rw;
 reg [7:0] internal_data;
-reg transaction_done = 0;
 //handshake
 always @ (posedge clk or negedge rst_n)begin
     if(!rst_n)begin
@@ -73,13 +67,10 @@ always @ (posedge clk or negedge rst_n)begin
         out_tx_ready <= 1;
         internal_rw <= 0;
         out_cs_n <= 1;
-        out_rx_valid <= 0;
         out_busy <= 0;
-        out_done <= 0;
     end else begin
         case(state)
             IDLE: begin
-                out_rx_valid <= 0;
                 out_tx_ready <= 1;
                 out_cs_n <= 1;
                 if(in_start) begin
@@ -92,92 +83,87 @@ always @ (posedge clk or negedge rst_n)begin
                 end
             end
             BUSY: begin
-                out_rx_valid <= 0;
                 out_tx_ready <= 0;
                 out_cs_n <= 0;
-                if(transaction_done) begin
-                    out_rx_valid <= 1;
-                    state <= FINISHED;
+                if(out_done) begin
+                    state <= (!internal_rw) ? IDLE : FINISHED; //go directly to idle if transaction was a write
                     out_cs_n <= 1;
                     out_busy <= 0;
-                    out_done <= 1;
                 end
             end
-            FINISHED: begin
-                out_rx_valid <= 1;
+            FINISHED: begin //wait for fsm to take in the data
                 out_tx_ready <= 0;
                 out_cs_n <= 1;
                 if(in_rx_ready) begin
-                    out_rx_valid <= 0;
                     out_tx_ready <= 1;
                     state <= IDLE;
                 end
             end
             default: begin
+                internal_data <= 0;
                 state <= IDLE;
                 out_tx_ready <= 1;
-                out_rx_valid <= 0;
+                internal_rw <= 0;
                 out_cs_n <= 1;
+                out_busy <= 0;
             end
         endcase
     end
 end
 
 
-//generate out_sclk
+//generate out_sclk and interact with flash
 always @ (posedge clk or negedge rst_n) begin
     if(!rst_n)begin
         out_sclk <= 0;
-        out_sclk_prev <= 0;
         out_sclk_counter <= 0;
+        out_done <= 0;
+        cycle_count <= 0;
+        out_rx_data <= 0;
+        io_ena <= 0;
+        out_io <= 0;
+        out_rx_valid <= 0;
     end else begin
         if(state == BUSY)begin
-            out_sclk_prev <= out_sclk;
-            if(out_sclk_counter == DIVIDER - 1)begin
+            if(out_sclk_counter == DIVIDER - 1 && !out_done)begin //toggle out_sclk
+                if(cycle_count == num_cycles)begin
+                    out_done <= 1;
+                    cycle_count <= 0;
+                    out_rx_valid <= 1;
+                end else begin
+                    out_done <= 0;
+                    out_rx_valid <= 0;
+                end
+                if(internal_rw && !out_done) begin //read on falling sclk edge
+                    if(out_sclk)begin
+                        cycle_count <= cycle_count + 1;
+                        if(quad_enable) out_rx_data <= {in_io[3:0], out_rx_data[7:4]}; //read 4 bits at a time
+                        else out_rx_data <= {in_io[0], out_rx_data[7:1]}; //read one bit at a time
+                    end
+                end else if (!internal_rw && !out_done)begin //write on rising sclk edge
+                    if (!out_sclk)begin
+                        io_ena <= 4'b1111;
+                        cycle_count <= cycle_count + 1;
+                        if(quad_enable) out_io[3:0] <= internal_data[7 - cycle_count*4 -: 4]; //shift out data on all 4 wires
+                        else out_io[0] <= internal_data[7 - cycle_count]; //shift data only on 1 wire
+                    end else io_ena <= 0;
+                end
                 out_sclk <= ~out_sclk;
                 out_sclk_counter <= 0;
             end else begin
                 out_sclk_counter <= out_sclk_counter + 1;
             end
-        end else begin
+        end else if (state == IDLE) begin
             out_sclk <= 0;
             out_sclk_counter <= 0;
-            out_sclk_prev <= 0;
-        end
-    end
-end
-
-//interact with flash
-always @ (posedge clk or negedge rst_n) begin
-    if(!rst_n)begin
-        transaction_done <= 0;
-        out_rx_data <= 0;
-        cycle_count <= 0;
-        io_ena <= 0;
-    end else if(state == BUSY)begin 
-        if(cycle_count == num_cycles - 1)begin
-            transaction_done <= 1;
+            out_done <= 0;
             cycle_count <= 0;
-        end else begin
-            cycle_count <= cycle_count + 1;
-            transaction_done <= 0;
+            out_rx_data <= 0;
+            cycle_count <= 0;
+            io_ena <= 0;
+            out_io <= 0;
+            out_rx_valid <= 0;
         end
-        if(!internal_rw) begin //read on rising sclk edge
-            if(out_sclk_rising)begin
-                if(quad_enable) out_rx_data <= {out_rx_data[3:0], in_io[3:0]}; //read 4 bits at a time
-                else out_rx_data <= {out_rx_data[6:0], in_io[0]}; //read one bit at a time
-            end
-        end else begin //write on falling sclk edge
-            if (out_sclk_falling)begin
-                io_ena <= 4'b1111;
-                if(quad_enable) out_io[3:0] <= internal_data[cycle_count*4 +: 4]; //shift out data on all 4 wires
-                else out_io[0] <= internal_data[cycle_count]; //shift data only on 1 wire
-            end else io_ena <= 0;
-        end
-    end else begin //idle or finished state
-        transaction_done <= 0;
-        cycle_count <= 0;
-        io_ena <= 0;
     end
 end
 endmodule
